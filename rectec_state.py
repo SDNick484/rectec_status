01523db0e07b0609d88baf2f04d517b0b6a7ab60
Nick Andrade <SDNick484@users.noreply.github.com>
2018-07-16 08:42:46 -0700

    Added comments about what I believe is happening

--- rectec_state.py
+++ rectec_state.py
@@ -1,16 +1,18 @@
 import json
 import pytuya
 
+# Specify the smoker and get its status
 d = pytuya.OutletDevice('<gwID>', '<IP>', '<productKey>')
 data = d.status()
 
+# Enable debug to see the raw JSON
 Debug = False
 #Debug = True
-
 if Debug:
     raw = json.dumps(data, indent=4)
     print(raw)
 
+# Simple if statement to check if the smoker is on
 rt_state = data['dps']['1']
 
 if rt_state:
@@ -18,8 +20,12 @@ if rt_state:
 else:
     print('RecTec is off')
 
+# The following values are based on observation
+# dps = '102' & '103' might be swapped
 print('Target Temperature: %r' % data['dps']['102'])
 print('Current Temperature: %r' % data['dps']['103'])
+# When smoker is off (data['dps']['1] = False)
+# values of probes might be based on last "on"
 print('Probe A Temperature: %r' % data['dps']['105'])
 print('Probe B Temperature: %r' % data['dps']['106'])
 
